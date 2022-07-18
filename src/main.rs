e501ead347cd38a1efc2375c9799a52ccbf95909
Arongkorn <rootblack45@gmail.com>
2022-07-18 11:47:22 +0700

    refactor: `Uniform` seemed to faster

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use rand::{prelude::Distribution, Rng, SeedableRng};
+use rand::{distributions::Uniform, prelude::Distribution, Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 
 #[derive(Debug, Clone, Copy)]
-struct Alphanumeric;
+pub struct Alphanumeric;
 
 impl Distribution<u8> for Alphanumeric {
     fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> u8 {
@@ -11,17 +11,23 @@ impl Distribution<u8> for Alphanumeric {
                 abcdefghijklmnopqrstuvwxyz\
                 0123456789\
                 !@#$%^&*";
-        loop {
-            let var = rng.next_u32() >> (32 - 7); // "Uniform" maybe better
-            if var < RANGE {
-                return GEN_ASCII_STR_CHARSET[var as usize];
-            }
-        }
+
+        let range = Uniform::new(0, RANGE);
+        let n = range.sample(rng);
+        GEN_ASCII_STR_CHARSET[n as usize]
     }
 }
 
 fn main() {
-    let mut rng = ChaCha8Rng::from_entropy(); // 8-round is enough?
-    let chars: String = (0..12).map(|_| rng.sample(Alphanumeric) as char).collect();
-    println!("{}", chars);
+    let mut rng = ChaCha8Rng::from_entropy(); // 8-round is enough?A
+
+    let mut incorrect = false;
+    for _ in 0..100 {
+        let c: char = rng.sample(Alphanumeric).into();
+        incorrect |= !(('0'..='9').contains(&c)
+            || ('A'..='Z').contains(&c)
+            || ('a'..='z').contains(&c)
+            || (['!', '@', '#', '$', '%', '^', '&', '*']).contains(&c));
+    }
+    assert!(!incorrect);
 }
